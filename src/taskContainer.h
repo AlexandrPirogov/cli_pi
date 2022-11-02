1512967c8341c5c87f197409dfde0238c766d8b6
AlexandrPirogov <devAlexandrPirogov@gmail.com>
2022-11-02 16:11:03 +0300

    added class TasksContainer

---
+++ src/taskContainer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace pi
+{
+    class TasksContainer
+    {
+        public:
+            TasksContainer() = default;
+
+            inline int size() noexcept
+            {
+                return 0;
+            }
+    };
+};
